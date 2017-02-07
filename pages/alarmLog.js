287dbfe1cc63c014f2bc08c214ed50044bb804e7
kfrancis2 <knf2@rice.edu>
2017-02-06 22:32:23 -0600

    lots of changes!

--- pages/alarmLog.js
+++ pages/alarmLog.js
@@ -6,8 +6,8 @@ window.onload = function () {
         storageBucket: "breaking-vad-online-simulation.appspot.com"
     };
     firebase.initializeApp(config);
-
-    var alarms = firebase.database().ref("alarmLog");
+    var uid = localStorage.getItem('uid');
+    var alarms = firebase.database().ref(uid + "/alarmLog");
     var sortedAlarms = [];
 
     var columns = ['alarm','rpm','Lmin','watts'];
@@ -20,7 +20,7 @@ window.onload = function () {
         var alarmEntries = snapshot.val();
         var table = document.getElementById("alarmTable");
         var row; var cell; var entry;
-        var entryKeys = Object.keys(alarmEntries);
+        // var entryKeys = Object.keys(alarmEntries);
         for(var entryKey in alarmEntries) {
             entry = alarmEntries[entryKey];
             sortedAlarms.push([entry, moment(entry['date'] + ',' + entry['onset'])]);
@@ -34,8 +34,16 @@ window.onload = function () {
             var sortedEntry = sortedAlarms[index][0];
             row = table.insertRow(0);
             row.insertCell(0).innerHTML = moment(sortedEntry['date']).format('MM/DD/YY');
-            row.insertCell(1).innerHTML = moment(sortedEntry['date'] + ',' + sortedEntry['onset']).format('HH:mm:ss');
-            row.insertCell(2).innerHTML = moment(sortedEntry['date'] + ',' + sortedEntry['resolved']).format('HH:mm:ss');
+            if(sortedEntry['onset']) {
+                row.insertCell(1).innerHTML = '--';
+            } else {
+                row.insertCell(1).innerHTML = moment(sortedEntry['date'] + ',' + sortedEntry['onset']).format('HH:mm:ss');
+            }
+            if(sortedEntry['resolved']) {
+                row.insertCell(2).innerHTML = '--';
+            } else {
+                row.insertCell(2).innerHTML = moment(sortedEntry['date'] + ',' + sortedEntry['resolved']).format('HH:mm:ss');
+            }
             for(var i=0; i<columns.length; i++) {
                 row.insertCell(i+3).innerHTML = sortedEntry[columns[i]];
             }
---
+++ pages/pumpPage.js
@@ -0,0 +1,155 @@
+function changeTab(id) {
+    if (id) {
+        document.getElementById(id).style.visibility = "visible";
+    } else {
+        document.getElementById('speedControl').style.visibility = "hidden";
+    }
+}
+
+function changeFPTab(idVis, idHide) {
+    if (document.getElementById(idHide)) {
+        console.log('if statement');
+        document.getElementById(idHide).id = idVis;
+        flowPowerCharts(idVis);
+    }
+}
+
+window.onload = function () {
+    document.getElementById("defaultOpen").click();
+    document.getElementById("flowChart").click();
+
+    var config = {
+        apiKey: "AIzaSyDHQ1wGhiNYdzBHIdb_mzMXfnyp0GdGnR8",
+        authDomain: "breaking-vad-online-simulation.firebaseapp.com",
+        databaseURL: "https://breaking-vad-online-simulation.firebaseio.com",
+        storageBucket: "breaking-vad-online-simulation.appspot.com"
+    };
+    firebase.initializeApp(config);
+    var uid = localStorage.getItem('uid');
+    var values = firebase.database().ref(uid + "/values/");
+
+    flowPowerCharts('flowChartContainer');
+
+    values.on('value', function(snapshot) {
+        var output = snapshot.val();
+        document.getElementById('rpmButton').innerHTML = output.RPM;
+    })
+};
+
+var fpProps = [
+    {axisX:{
+        title : "Time (s)",
+        titleFontColor: "blue",
+        titleFontFamily: "arial",
+        labelFontFamily: "arial",
+        lineColor: "black",
+        lineThickness: 5,
+        tickColor: "black",
+        labelFontColor: "black",
+        // labelFontSize: 1
+    },
+        axisY:{
+            title : "Flow (L/min)",
+            titleFontColor: "blue",
+            titleFontFamily: "arial",
+            labelFontFamily: "arial",
+            lineColor: "black",
+            lineThickness: 5,
+            minimum: 0,
+            maximum: 10,
+            tickColor: "black",
+            gridColor: "white",
+            labelFontColor: "black",
+            labelFontSize: 10
+    }},
+    {axisX:{
+            title : "Time (s)",
+            titleFontColor: "red",
+            titleFontFamily: "arial",
+            labelFontFamily: "arial",
+            lineColor: "black",
+            lineThickness: 5,
+            tickColor: "black",
+            labelFontColor: "black",
+            // labelFontSize: 0
+        },
+    axisY: {
+        title : "Power (Watts)",
+            titleFontColor: "red",
+            titleFontFamily: "arial",
+            labelFontFamily: "arial",
+            lineColor: "black",
+            lineThickness: 5,
+            minimum: 0,
+            maximum: 15,
+            tickColor: "black",
+            gridColor: "white",
+            labelFontColor: "black",
+            labelFontSize: 10
+    }}];
+
+function flowPowerCharts(idVis) {
+    if (idVis == 'flowChartContainer') {var fp = 0;} else {fp = 1;}
+
+    var data = []; // dataPoints
+    var flowOrPower;
+    var amplitude;
+    var fpChart = new CanvasJS.Chart(idVis,{
+        toolTip:{
+            enabled: false
+        },
+        data: [{
+            type: "line",
+            dataPoints: data,
+            color: "black"
+        }],
+        axisX: fpProps[fp].axisX,
+        axisY: fpProps[fp].axisY
+    });
+
+    var tVal = 0;
+    var updateInterval = 100;
+    var dataLength = 200; // number of dataPoints visible at any point
+
+    var values = firebase.database().ref("values/");
+
+    values.on('value', function(snapshot) {
+        var output = snapshot.val();
+
+        var fpStrings = ['flowrate','power'];
+        flowOrPower = parseFloat(output[fpStrings[fp]]);
+        fpStrings = ['flowAmplitude','powerAmplitude'];
+        amplitude = parseFloat(output[fpStrings[fp]]);
+        var offsetVals = [[-0.9, -0.8, -0.7, -0.2, 0.3, 0.8, 1.2, 1.2, 0.8, 0.3, -0.2, -0.7],[-0.2, -0.2, -0.1, 0, 0.2, 0.5, 0.6, 0.7, 0.5, 0.1, 0, -0.1]];
+        var i = 0;
+
+        var updateChart = function (count) {
+            count = count || 1;
+            // count is number of times loop runs to generate dataPoints.
+
+            for (var j = 0; j < count; j++) {
+
+                data.push({
+                    x: tVal,
+                    y: flowOrPower + amplitude*offsetVals[fp][i] + .15*Math.random()
+                });
+                tVal = tVal + .3;
+                if (data.length > dataLength) {
+                    data.shift();
+                }
+            }
+
+            fpChart.render();
+            if(i<11) {
+                i++;
+            } else {
+                i = 0;
+            }
+        };
+        // generates first set of dataPoints
+        updateChart(dataLength);
+
+        // update chart after specified time.
+        window.setInterval(function(){updateChart()}, updateInterval);
+    });
+}
